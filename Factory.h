90f2391cfb22ddaaab5235fa5529b1ef7fec7c62
samibadra <samibadra@yahoo.com>
2015-05-23 23:29:21 -0700

    Update Factory.h

--- Factory.h
+++ Factory.h
@@ -7,6 +7,10 @@
  FILE: Factory.h
  
  Copyright (c) 2015 Sami Badra. All rights reserved.
+ 
+ DISCLAIMER: Any unauthorized use, including but not limited to, copying or
+ redistributing any chunk of the source code (or an entire file) will result in
+ punishment by law. I, Sami Badra, own all rights to the files and their contents.
  */
 
 #ifndef __Assignment3__Factory__
