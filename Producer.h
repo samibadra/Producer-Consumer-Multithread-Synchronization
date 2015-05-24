046593e41debb72e78814ee383770a0c05f4db3c
samibadra <samibadra@yahoo.com>
2015-05-23 23:23:20 -0700

    Create Producer.h

---
+++ Producer.h
@@ -0,0 +1,19 @@
+/*
+ Sami Badra
+ Enrique Leon
+ 
+ CS 570, Spring 2015
+ Assignment #3, producer/consumer simulation
+ FILE: Producer.h
+ 
+ Copyright (c) 2015 Sami Badra. All rights reserved.
+ */
+
+#ifndef __Assignment3__Producer__
+#define __Assignment3__Producer__
+
+#include <stdio.h>
+
+void *produceCandy(void *arg);
+
+#endif /* defined(__Assignment3__Producer__) */
