6026b9548ae3c3a5b2ad654aebc76cd07802c0e5
samibadra <samibadra@yahoo.com>
2015-05-23 23:24:32 -0700

    Create Consumer.h

---
+++ Consumer.h
@@ -0,0 +1,20 @@
+/*
+ Sami Badra
+ Enrique Leon
+ 
+ CS 570, Spring 2015
+ Assignment #3, producer/consumer simulation
+ FILE: Consumer.h
+ 
+ Copyright (c) 2015 Sami Badra. All rights reserved.
+ */
+
+#ifndef __Assignment3__Consumer__
+#define __Assignment3__Consumer__
+
+#include <stdio.h>
+
+void *consumeCandy(void *arg);
+
+#endif /* defined(__Assignment3__Consumer__) */
+
