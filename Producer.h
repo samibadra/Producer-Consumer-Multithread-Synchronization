090cd86d63c570e26816630d55c3563904ab88c9
Sami Badra <samibadra@yahoo.com>
2015-05-29 04:46:58 -0700

    Update Producer.h

--- Producer.h
+++ Producer.h
@@ -7,10 +7,6 @@
  FILE: Producer.h
  
  Copyright (c) 2015 Sami Badra. All rights reserved.
- 
- DISCLAIMER: Any unauthorized use, including but not limited to, copying or
- redistributing any chunk of the source code (or an entire file) will result in
- punishment by law. I, Sami Badra, own all rights to the files and their contents.
  */
 
 #ifndef __Assignment3__Producer__
